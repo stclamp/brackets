14d70d50c57f8e33a1e10e94a91cdac505e06d4e
Pavel <whatisclamp@gmail.com>
2022-07-02 20:30:16 +0300

    final solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let configArr = [];
+  let result = str;
+  bracketsConfig.map(item => {
+    configArr.push(item.join(''))
+  })
+
+
+  for (let i = 0; i < str.length; i = i + 2) {
+    for (k in configArr) {
+      if (!result) {
+        continue
+      } else {
+        result = result.replace(configArr[k], '')
+      }
+    }
+
+    if (!result) {
+      continue
+    } else {
+      result = result
+    }
+  }
+
+  return result ? false : true
 }

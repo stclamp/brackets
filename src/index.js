8c3ffa38091aa01537e4b1001ac8b2174f630a19
Pavel <whatisclamp@gmail.com>
2022-07-02 20:31:44 +0300

    fix: remove useful strings

--- src/index.js
+++ src/index.js
@@ -8,17 +8,7 @@ module.exports = function check(str, bracketsConfig) {
 
   for (let i = 0; i < str.length; i = i + 2) {
     for (k in configArr) {
-      if (!result) {
-        continue
-      } else {
-        result = result.replace(configArr[k], '')
-      }
-    }
-
-    if (!result) {
-      continue
-    } else {
-      result = result
+      result = result.replace(configArr[k], '')
     }
   }
 
